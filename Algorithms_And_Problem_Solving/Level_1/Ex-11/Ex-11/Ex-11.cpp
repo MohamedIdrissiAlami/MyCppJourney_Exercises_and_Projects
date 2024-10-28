fda21e39e86d6526b699a4cd1cc3c9ae0e35bb8c
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-10-28 22:03:36 +0100

    Ex-11

---
+++ Algorithms_And_Problem_Solving/Level_1/Ex-11/Ex-11/Ex-11.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T ReadNumber(string Message)
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't input a valid number .
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\ninvalid input " << Message;
+
+	}
+	return Number;
+}
+template <typename T>
+T ReadPositiveNumber(string Message)
+{
+	T Number = 0;
+	do
+	{
+		if ((Number = ReadNumber<T>(Message)) < 0)
+			cout << "try again with a positive number..";
+
+	} while (Number < 0);
+	return Number;
+}
+template <typename T>
+T ReadNumberInRange(T From, T To, string Message = "")
+{
+	T Number = 0;
+	do
+	{
+		Number = ReadNumber<T>(Message);
+	} while (Number<From || Number>To);
+	return Number;
+}
+
+template <typename T>
+T Sum(T Number1, T Number2, T Number3)
+{
+	return Number1 + Number2 + Number3;
+}
+
+
+float AverageOf3Marks(short Mark1, short Mark2, short Mark3)
+{
+	return (float)Sum<short>(Mark1, Mark2, Mark3) / 3;
+}
+
+bool IsPass(float Average)
+{
+	return Average >= 50;
+}
+
+int main()
+{
+	cout << "\nEnter 3 markes : ";
+	short Mark1 = ReadNumberInRange<short>(0, 100, "\nNumber 1 = "), Mark2 = ReadNumberInRange<short>(0, 100, "\nNumber 2 = "), Mark3 = ReadNumberInRange<short>(0, 100, "\nNumber 3 = ");
+	float Average = AverageOf3Marks(Mark1, Mark2, Mark3);
+	
+	cout << "\n" << "Average : " << Average <<"\n" << (IsPass(Average) ? "Pass" : "Fail") << endl;
+	system("pause>0");
+	return 0;
+}
---
+++ Algorithms_And_Problem_Solving/Level_1/Ex-11/Ex-11/readme.md
@@ -0,0 +1,11 @@
+## Ex-11:Average pass fail: 
+Write a Cpp program to ask the user to enter :  
+- mark1,mark2,mark3  
+Then print the avarage of the entered marks and print `pass` if average >= 50,otherwise print `fail`  
+### Exemple : 
+90
+80
+70  
+-> output : 
+80
+pass.
