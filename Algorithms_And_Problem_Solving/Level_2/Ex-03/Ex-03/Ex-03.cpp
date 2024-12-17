9d31457e5a55199d97910305da0c10c80b2c48a8
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-12-17 07:19:33 +0100

    #03/02

---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-03/Ex-03/Ex-03.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't enter a valid number ...
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nInvalid input try again ..";
+		cin >> Number;
+	}
+	return Number;
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "\nEnter a positive Number : ")
+{
+	T Number = 0;
+	while ((Number = ReadNumber<T>(Message)) < 0)
+	{
+		cout << "\nyou have entered a negative number try again with a positive input!";
+	}
+	return Number;
+}
+
+bool IsPerfect(short Number)
+{
+	short SumOfDivisors = 0,HalfOfNumber=ceil(Number/2);
+	for (short i = 1; i <= HalfOfNumber; i++)
+	{
+		if (Number % i == 0)
+		{
+			SumOfDivisors += i;
+		}
+	}
+	return (Number == SumOfDivisors) ? true : false;
+}
+
+int main()
+{
+	short Number = ReadPositiveNumber<short>("\nEnter a number : ");
+	cout << "\n" << (IsPerfect(Number) ? "It's perfect." : "It's not perfect.") << endl;
+	system("pause>0");
+	return 0;
+
+}
---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-03/Ex-03/readme.md
@@ -0,0 +1,12 @@
+## Ex-03 : Is it a perfect number: 
+
+Write a program to check if a given number is perfect or not .
+
+### Note : 
+__Perfect number = sum of all its divisors.__
+
+### Example : 
+ 
+* 28 = 1+2+4+7+14 -> 28 is perfect.
+* 24 != 1+2+3+4+6+8+12 -> 24 is not perfect.
+
