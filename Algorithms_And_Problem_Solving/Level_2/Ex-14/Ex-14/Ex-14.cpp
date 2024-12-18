f068df8a96f1638c9d1359a5f195136b5c715462
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-12-18 10:26:01 +0100

    #14/02

---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-14/Ex-14/Ex-14.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't enter a valid number ...
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nInvalid input try again ..";
+		cin >> Number;
+	}
+	return Number;
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "\nEnter a positive Number : ")
+{
+	T Number = 0;
+	while ((Number = ReadNumber<T>(Message)) < 0)
+	{
+		cout << "\nyou have entered a negative number try again with a positive input!";
+	}
+	return Number;
+}
+
+void PrintInvertedLetterPattern(short Number)
+{
+	for (short i = Number; i >= 1; i--)
+	{
+		for (short j = i; j >= 1; j--)
+		{
+			cout << char(i + 64) << " ";
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	PrintInvertedLetterPattern(ReadPositiveNumber<short>());
+	system("pause>0");
+	return 0;
+}
---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-14/Ex-14/readme.md
@@ -0,0 +1,10 @@
+## Ex-14 Inverted letter pattern:  
+Write a program to read a number and print inverted letter pattern as follows :  
+Input :  
+5  
+=> Output:  
+E E E E E  
+D D D D  
+C C C  
+B B  
+A  
