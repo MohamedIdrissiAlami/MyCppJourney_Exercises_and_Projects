e5afde7d6f6e467b78b07850e9c0475701da85a2
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-12-18 10:24:56 +0100

    #11/02

---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-11/Ex-11/Ex-11.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't enter a valid number ...
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nInvalid input try again ..";
+		cin >> Number;
+	}
+	return Number;
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "\nEnter a positive Number : ")
+{
+	T Number = 0;
+	while ((Number = ReadNumber<T>(Message)) < 0)
+	{
+		cout << "\nyou have entered a negative number try again with a positive input!";
+	}
+	return Number;
+}
+
+int ReverseNumber(int Number)
+{
+	int RevNumber = 0;
+	while (Number > 0)
+	{
+		RevNumber = RevNumber * 10 + Number % 10;
+		Number /= 10;
+	}
+	return RevNumber;
+}
+
+bool IsPalindrome(int Number)
+{
+	return Number == ReverseNumber(Number);
+}
+int  main()
+{
+	int Number = ReadPositiveNumber<int>("\nEnter a number : ");
+	cout << (IsPalindrome(Number)? "\nYes,it is a palindrome number." :
+		"\nNo ,It is not a palindrome number.") << endl;
+	system("pause>0");
+	return 0;
+}
---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-11/Ex-11/readme.md
@@ -0,0 +1,11 @@
+## Ex 11 : palindrome number : 
+Write a program to read number to check if it is palindrome  
+A Plandrome number is that reads the same from right to left.  
+
+### Example :  
+input :  
+12334  
+12321  
+=> Output :  
+No,it is not a palindrome number.  
+Yes, it is a palindrome number.  
