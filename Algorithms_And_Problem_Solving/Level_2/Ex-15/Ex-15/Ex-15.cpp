8adcde4468ccb7b4d858139a4b95c2400e4811d0
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-12-18 10:26:17 +0100

    #15/02

---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-15/Ex-15/Ex-15.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't enter a valid number ...
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nInvalid input try again ..";
+		cin >> Number;
+	}
+	return Number;
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "\nEnter a positive Number : ")
+{
+	T Number = 0;
+	while ((Number = ReadNumber<T>(Message)) < 0)
+	{
+		cout << "\nyou have entered a negative number try again with a positive input!";
+	}
+	return Number;
+}
+
+void PrintLetterPattern(short Number)
+{
+	for (short i = 1; i <= Number; i++)
+	{
+		for (short j = 1; j <= i; j++)
+		{
+			cout << char(i + 64) << " ";
+		}
+		cout << "\n";
+	}
+}
+
+int main()
+{
+	PrintLetterPattern(ReadPositiveNumber<short>("\nEnter a number : "));
+	system("pause>0");
+	return 0;
+}
---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-15/Ex-15/readme.md
@@ -0,0 +1,13 @@
+## Ex-15 : letter pattern  : 
+
+Write a program to read a number and print letter pattern as follows :  
+
+Input :   
+5  
+=> Output  :  
+
+A  
+B B  
+C C C  
+D D D D  
+E E E E E  
