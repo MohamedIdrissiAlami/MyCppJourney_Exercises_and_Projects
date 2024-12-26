9f42a254f972169ae759941bb4d6535c92f0227d
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-12-26 21:15:03 +0100

    #34/02

---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-34/Ex-34/Ex-34.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't enter a valid number ...
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nInvalid input try again ..";
+		cin >> Number;
+	}
+	return Number;
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "\nEnter a positive Number : ")
+{
+	T Number = 0;
+	while ((Number = ReadNumber<T>(Message)) < 0)
+	{
+		cout << "\nyou have entered a negative number try again with a positive input!";
+	}
+	return Number;
+}
+
+short RandomNumber(short From, short To)
+{
+	return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumber(short arr[], short arrLength)
+{
+	for (short i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(short arr[], short arrLength)
+{
+	for (short i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+template<typename T>
+T GetElementIndexInArray(T arr[],T ElementToCheck, short arrLength)
+{
+	for (short i = 0; i < arrLength; i++)
+	{
+		if (arr[i] == ElementToCheck)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	short arrLength = ReadPositiveNumber<short>("\nEnter Array length : ");
+	short* arr = new short[arrLength];
+	FillArrayWithRandomNumber(arr, arrLength);
+	cout << "\nthe following are array elements : \n";
+	PrintArray(arr, arrLength);
+	short NumberToCheck = ReadNumber<short>("\nEnter a number to search for : ");
+	short NumberIndex = 0;
+	if ((NumberIndex = GetElementIndexInArray<short>(arr, NumberToCheck, arrLength)) != -1)
+		cout << "\n" << "the number was found at position : " << NumberIndex << endl;
+	else
+		cout<<"\nthe number you're looking does'nt exist!"<< endl;
+
+
+	system("pause>0");
+	return 0;
+
+}
---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-34/Ex-34/readme.md
@@ -0,0 +1,13 @@
+## Ex-34 : return number index in array : 
+
+Write a program to fill an array with random numbers from 1 to 100 ,  
+read number and return its index in the array if found.  
+Otherwise return -1 .  
+### Example :  
+Input :  
+10  
+=> output : array elements :  
+10 1 22 88 20 30 78 99 66 50  
+Please enter a number to search for ?  
+88  
+the number was found at position 3.  
