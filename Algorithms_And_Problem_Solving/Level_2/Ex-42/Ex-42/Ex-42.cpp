21e3a4c8f10f474865d33bd4248495940d9eb534
mohamedidrissialami <mohamedidrissialami22@gmail.com>
2024-12-28 16:26:04 +0100

    #42/02

---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-42/Ex-42/Ex-42.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't enter a valid number ...
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nInvalid input try again ..";
+		cin >> Number;
+	}
+	return Number;
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "\nEnter a positive Number : ")
+{
+	T Number = 0;
+	while ((Number = ReadNumber<T>(Message)) < 0)
+	{
+		cout << "\nyou have entered a negative number try again with a positive input!";
+	}
+	return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+	return rand() % (To - From + 1) + From;;
+}
+
+void PrintArray(short arr[], short arrLength)
+{
+	for (short i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+void FillArrayWithRandomNumbers(short arr[], short arrLength)
+{
+	for (short i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+bool IsOdd(short Number)
+{
+	return (Number % 2) != 0;
+}
+
+short OddNumbersCountInArray(short arr[100], short arrLength)
+{
+	short OddNumbersCount = 0;
+	for (short i = 0; i < arrLength; i++)
+	{
+		if (IsOdd(arr[i]))
+		{
+			++OddNumbersCount;
+		}
+	}
+	return OddNumbersCount;
+}
+
+int main()
+{
+	//seeds the random number generator in C++,called only once.
+	srand((unsigned)time(NULL));
+	short arrLength = ReadPositiveNumber<short>("\nHow many numbers do you wanna generate : ");
+	short* arr = new short[arrLength];
+	FillArrayWithRandomNumbers(arr, arrLength);
+	cout << "\nthe following are array elements : ";;
+	PrintArray(arr, arrLength);
+
+	cout << "\nOdd numbers count : " << OddNumbersCountInArray(arr, arrLength);
+
+	delete[]arr;
+
+	system("pause>0");
+
+	return 0;
+}
---
+++ Algorithms_And_Problem_Solving/Level_2/Ex-42/Ex-42/readme.md
@@ -0,0 +1,9 @@
+## Ex-42 : count odd numbers in arrays :  
+Write a program to fill array with random numbers from 1 to 100 ,then print the count of odd numbers.  
+### Example :  
+input : 
+6
+
+=>  
+array elements : 12 23 54 8 99 66  
+odd numbers count is : 2  
