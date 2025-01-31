ea134137992d8c04388395b7f768cae6bc621e09
MOHAMED IDRISSI ALAMI <mohamedidrissialami3@gmail.com>
2025-01-31 12:40:37 +0100

    Ex-14 : Date1 equals date2

---
+++ Algorithms_And_Problem_Solving/Level_4/Ex-14/Ex-14.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+#include <string >
+using namespace std;
+
+struct stDate
+{
+	short Year;
+	short Month;
+	short Day;
+};
+
+template <typename T>
+T ReadNumber(string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	while ((cin >> Number).fail())
+	{
+		//user didn't input a valid number 
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\ninvalid input ! try again with a valid number ...";
+		cin >> Number;
+	}
+	return Number;
+
+}
+
+template <typename T>
+T ReadPositiveNumber(string Message = "")
+{
+	T Number = 0;
+	do
+	{
+		if ((Number = ReadNumber<T>(Message)) < 0)
+			cout << "\nout of range! try again..";
+	} while (Number < 0);
+	return Number;
+}
+
+
+template <typename T>
+T ReadNumberBetween(T From, T To, string Message = "")
+{
+	T Number = 0;
+	cout << Message;
+	do
+	{
+		if ((Number = ReadNumber<T>(Message)) < From || Number > To)
+			cout << "\nout of range! try again..";
+	} while (Number<From || Number>To);
+	return Number;
+}
+
+bool IsLeapYear(short Year)
+{
+	return Year % 100 == 0 ? Year % 400 == 0 : Year % 4 == 0;
+}
+
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+	if (Month < 1 || Month>12)
+		return 0;
+	short MonthDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : MonthDays[Month - 1];
+}
+bool IsValidYear(short Year)
+{
+	return Year > 1000 && Year < 3000;
+}
+
+bool IsValidMonth(short Month)
+{
+	return Month > 0 && Month <= 12;
+}
+
+bool IsValidDay(short Year, short Month, short Day)
+{
+	return Day >= 0 && Day <= NumberOfDaysInMonth(Year, Month);
+}
+
+bool IsValidDate(stDate Date)
+{
+	return IsValidYear(Date.Year) && IsValidMonth(Date.Month) && IsValidDay(Date.Year, Date.Month, Date.Day);
+}
+
+short ReadYear()
+{
+	short Year = 0;
+	while (!IsValidYear(Year = ReadNumber<short>("\nEnter Year ? ")))
+	{
+		cout << "\ninvalid Year! try again..";
+	}
+	return Year;
+}
+
+short ReadMonth()
+{
+	short Year = 0;
+	while (!IsValidMonth(Year = ReadNumber<short>("\nEnter Month ? ")))
+		cout << "\ninvalid month! try again..";
+	return Year;
+}
+
+short ReadDay(short Year, short Month)
+{
+	short Day = 0;
+	while (!IsValidDay(Year, Month, Day = ReadNumber<short>("\nEnter Day ? ")))
+		cout << "\ninvalid day! try again..";
+	return Day;
+}
+
+stDate ReadDate(string Message = "")
+{
+	cout << Message;
+	stDate Date;
+	Date.Year = ReadYear();
+	Date.Month = ReadMonth();
+	Date.Day = ReadDay(Date.Year, Date.Month);
+	return Date;
+
+}
+
+string DateString(stDate Date)
+{
+	return to_string(Date.Day) + "/" + to_string(Date.Month) + "/" + to_string(Date.Year);
+}
+
+bool IsDate1EqualDate2(stDate Date1, stDate Date2)
+{
+	return ((Date1.Year == Date2.Year) ? ((Date1.Month == Date2.Month) ? (Date1.Day == Date1.Day) : false) : false);
+}
+
+
+int main()
+{
+	stDate Date1 = ReadDate("\n\nEnter date 1 :\n");
+	stDate Date2 = ReadDate("\n\nEnter date 2 : \n");
+	IsDate1EqualDate2(Date1, Date2) ? cout << "\nyes,date 1 is equal to date 2 ." : cout << "\nNo,date 1 is not equal to date2.";
+
+	system("pause>0");
+	return 0;
+}
---
+++ Algorithms_And_Problem_Solving/Level_4/Ex-14/readme.md
@@ -0,0 +1,15 @@
+## Ex-14 : Date1 equals date 2 :  
+Write a program to read `date1` and `Date2` then check if the date 1 equals date 2  
+-> output :  
+
+* Date1 :  
+please enter a day : 10  
+Please enter a month : 10  
+please enter a year : 2022  
+
+* Date2 :  
+please enter a day : 10  
+Please enter a month : 10  
+please enter a year : 2022  
+
+yes,date1 equals date2.  
